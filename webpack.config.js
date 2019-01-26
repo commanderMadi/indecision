1e2507980ed3c8449b3f08967f18d1805fd65af0
Darth Coder <00darthcoder00@gmail.com>
2019-01-26 14:04:48 +0200

    Fix webpack env error

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public', 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },
-
-            {   use: ['style-loader', 'css-loader', 'sass-loader'],
-                test:/\.s?css$/
-            }
-        ]
-    },
-    devtool: env === 'production' ? 'source-map' : 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        publicPath: '/dist/',
-        historyApiFallback: true
+module.exports = env => {
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+    
+                {   use: ['style-loader', 'css-loader', 'sass-loader'],
+                    test:/\.s?css$/
+                }
+            ]
+        },
+        devtool: env === 'production' ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            publicPath: '/dist/',
+            historyApiFallback: true
+        }
     }
 }
